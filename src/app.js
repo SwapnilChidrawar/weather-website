7ae400976749533decbee19669ce088f9a3a5184
Swapnil Chidrawar <swapnil_chidrawar@persistent.co.in>
2019-07-26 18:09:57 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,86 @@
+const path = require('path');
+
+const express = require('express');
+const hbs = require('hbs');
+
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static('public'));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Swapnil Chidrawar empty'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Swapnil Chidrawar About'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        forecast: '50 degree',
+        title: 'Help',
+        helpText: 'This is a help page',
+        name: 'Swapnil Chidrawar'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please provide the address'
+        })
+    }
+    geoCode(req.query.address, (error, {latitude, longitude, location} = {} ) => {
+        if (error) {
+            return res.send({
+                error: error
+            })
+        } 
+        forecast(latitude, longitude, (error, forecast) => {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            } 
+            res.send({
+                location: location,
+                forecast: forecast
+            });
+        });
+    
+    });
+}); 
+
+app.get('/help/*', (req, res) => {
+    res.render('pageNotFound', {
+        title: 'Page Not Found',
+        name: 'Swapnil Chidrawar'
+    })
+});
+
+app.get('*', (req, res) => {
+    res.render('pageNotFound', {
+        title: 'Page Not Found',
+        name: 'Swapnil Chidrawar'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Web Server is listing on 3000 port...')
+});
